ad05b622d5b7cdcda38abcec46e7b0e25b5bc6e8
Andreas Plesch <andreasplesch@netscape.net>
2017-07-08 18:11:36 -0400

    interpolation keys via custom easing for tween

--- dist/aframe-keyframes-component.js
+++ dist/aframe-keyframes-component.js
@@ -62,6 +62,64 @@
 	  'bezier': AFRAME.TWEEN.Interpolation.Bezier,
 	  'catmullrom': AFRAME.TWEEN.Interpolation.CatmullRom
 	};
+	var EASING_FUNCTIONS = {
+	  'linear': AFRAME.TWEEN.Easing.Linear.None,
+
+	  'ease': AFRAME.TWEEN.Easing.Cubic.InOut,
+	  'ease-in': AFRAME.TWEEN.Easing.Cubic.In,
+	  'ease-out': AFRAME.TWEEN.Easing.Cubic.Out,
+	  'ease-in-out': AFRAME.TWEEN.Easing.Cubic.InOut,
+
+	  'ease-cubic': AFRAME.TWEEN.Easing.Cubic.In,
+	  'ease-in-cubic': AFRAME.TWEEN.Easing.Cubic.In,
+	  'ease-out-cubic': AFRAME.TWEEN.Easing.Cubic.Out,
+	  'ease-in-out-cubic': AFRAME.TWEEN.Easing.Cubic.InOut,
+
+	  'ease-quad': AFRAME.TWEEN.Easing.Quadratic.InOut,
+	  'ease-in-quad': AFRAME.TWEEN.Easing.Quadratic.In,
+	  'ease-out-quad': AFRAME.TWEEN.Easing.Quadratic.Out,
+	  'ease-in-out-quad': AFRAME.TWEEN.Easing.Quadratic.InOut,
+
+	  'ease-quart': AFRAME.TWEEN.Easing.Quartic.InOut,
+	  'ease-in-quart': AFRAME.TWEEN.Easing.Quartic.In,
+	  'ease-out-quart': AFRAME.TWEEN.Easing.Quartic.Out,
+	  'ease-in-out-quart': AFRAME.TWEEN.Easing.Quartic.InOut,
+
+	  'ease-quint': AFRAME.TWEEN.Easing.Quintic.InOut,
+	  'ease-in-quint': AFRAME.TWEEN.Easing.Quintic.In,
+	  'ease-out-quint': AFRAME.TWEEN.Easing.Quintic.Out,
+	  'ease-in-out-quint': AFRAME.TWEEN.Easing.Quintic.InOut,
+
+	  'ease-sine': AFRAME.TWEEN.Easing.Sinusoidal.InOut,
+	  'ease-in-sine': AFRAME.TWEEN.Easing.Sinusoidal.In,
+	  'ease-out-sine': AFRAME.TWEEN.Easing.Sinusoidal.Out,
+	  'ease-in-out-sine': AFRAME.TWEEN.Easing.Sinusoidal.InOut,
+
+	  'ease-expo': AFRAME.TWEEN.Easing.Exponential.InOut,
+	  'ease-in-expo': AFRAME.TWEEN.Easing.Exponential.In,
+	  'ease-out-expo': AFRAME.TWEEN.Easing.Exponential.Out,
+	  'ease-in-out-expo': AFRAME.TWEEN.Easing.Exponential.InOut,
+
+	  'ease-circ': AFRAME.TWEEN.Easing.Circular.InOut,
+	  'ease-in-circ': AFRAME.TWEEN.Easing.Circular.In,
+	  'ease-out-circ': AFRAME.TWEEN.Easing.Circular.Out,
+	  'ease-in-out-circ': AFRAME.TWEEN.Easing.Circular.InOut,
+
+	  'ease-elastic': AFRAME.TWEEN.Easing.Elastic.InOut,
+	  'ease-in-elastic': AFRAME.TWEEN.Easing.Elastic.In,
+	  'ease-out-elastic': AFRAME.TWEEN.Easing.Elastic.Out,
+	  'ease-in-out-elastic': AFRAME.TWEEN.Easing.Elastic.InOut,
+
+	  'ease-back': AFRAME.TWEEN.Easing.Back.InOut,
+	  'ease-in-back': AFRAME.TWEEN.Easing.Back.In,
+	  'ease-out-back': AFRAME.TWEEN.Easing.Back.Out,
+	  'ease-in-out-back': AFRAME.TWEEN.Easing.Back.InOut,
+
+	  'ease-bounce': AFRAME.TWEEN.Easing.Bounce.InOut,
+	  'ease-in-bounce': AFRAME.TWEEN.Easing.Bounce.In,
+	  'ease-out-bounce': AFRAME.TWEEN.Easing.Bounce.Out,
+	  'ease-in-out-bounce': AFRAME.TWEEN.Easing.Bounce.InOut
+	};
 
 
 	/**
@@ -82,7 +140,7 @@
 	    repeat: {default: 0}, //  Repeat count or indefinite.   0
 	    to: {default: []}, // must be specified, comma separated
 	    interpolation: {default: 'linear'}, // kind of interpolation, linear, bezier, catmullrom
-	    keys: {default: []} // must be specified, comma separated array of same length as to
+	    keys: {default: ['0', '1']} // must be specified, comma separated array of length of to plus one for from value
 	  },
 
 	  /**
@@ -140,10 +198,19 @@
 	    else {
 	      ani.setAttribute('to', type); // will be replaced, hopefully satifisfies a-animation init and start
 	    }
+
 	    // sanitize interpolation
 	    this.interpolation = INTERPOLATION_FUNCTIONS[data.interpolation] || INTERPOLATION_FUNCTIONS['linear'];
+
 	    // sanitize keys
 	    // check length and content
+	    this.keys = data.keys.map(parseFloat).map(function(v){return isNaN(v) ? 1:v ;});
+	    //sort, deduplicate
+	    if (this.keys.length > 1) {
+	      this.keys = this.keys.sort(function cmp(a,b) { return a-b ; }).
+	        filter(function(v, i, a){return v !== a[i-1];}) ;
+	    }
+	    if (this.keys.length === 0) {this.keys = [0, 1] ;}
 
 	    if (this.hasAnimationNode) { this.el.replaceChild(ani, this.animation);}
 	    else {
@@ -210,6 +277,9 @@
 	    tween.to(to, parseFloat(data.dur));
 	    tween.interpolation(this.interpolation);
 	    //interpolation keys, custom function
+	    var parsedEasing = EASING_FUNCTIONS[data.easing];
+	    tween.easing(keyedEasing(this.keys), parsedEasing);
+
 	    tween.start();
 
 	    // helper
@@ -220,6 +290,32 @@
 	      //if (t == 'number') {return v ;}
 	      if (t == 'boolean') { return v ? 1 : 0 ;}
 	      return parseFloat(v.toString()) ; // hopefully never happens
+	    }
+
+	    function keyedEasing(keys, easing) {
+	      //easing is applied on top
+	      easing = easing || AFRAME.TWEEN.Easing.Linear.None; //perhaps test if actually a number returning function
+
+	      return function piecewiseLinearEasingFunction (k) { // this is what tween expects as custom easing function
+
+	        var m = keys.length - 1;
+	        var fn = AFRAME.TWEEN.Interpolation.Utils.Linear;
+
+	        if (k <= keys[0]) {
+	          return easing(k); //fn(0, 1/m, k); // extrapolate
+	        }
+
+	        if (k >= keys[m]) {
+	          return easing(k); //fn( (m-1)/m, 1, k - keys[m]); // extrapolate
+	        }
+
+	        // find interval
+	        var right = keys.findIndex(function (key) {return key > k ;}); // could be smarter: start searching from previous
+	        var left = right - 1;
+	        var f = (k-keys[left])/(keys[right]-keys[left]);
+	        //the interpolation between left/m, right/m works because tween divides the duration evenly into 1/m wide pieces
+	        return easing(fn(left/m, right/m, f));
+	      };
 	    }
 
 	  },
